6b49b0903b4d3c44130959a690c411c063e56ef3
Seb Patin <Seb Patin>
2022-05-03 18:41:12 -0400

    gravity and enemies done

--- index.js
+++ index.js
@@ -6,20 +6,28 @@ canvas.height = 576
 
 c.fillRect(0,0, canvas.width, canvas.height)
 
+const gravity = 0.2
+
 class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
+        this.height = 150
     }
 
     draw() {
         c.fillStyle = "red"
-        c.fillRect(this.position.x, this.position.y, 50,150)
+        c.fillRect(this.position.x, this.position.y, 50,this.height)
 
     }
     update() {
         this.draw()
+        
         this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else this.velocity.y += gravity
     }
 }
 
@@ -41,7 +49,7 @@ const enemy = new Sprite({
     },
     velocity: {
         x: 0,
-        y: 10
+        y: 0
     }
 })
 
