31525e9d743c96252a4750aa293024c49d0e4b77
Seb Patin <Seb Patin>
2022-05-07 17:32:20 -0400

    shop sprite and animation complete

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ const background = new Sprite ({
     imageSrc: "./images/background.png"
 })
 
+const shop = new Sprite ({
+    position: {
+        x:600,
+        y:128
+    },
+    imageSrc: "./images/shop.png",
+    scale: 2.75,
+    framesMAX: 6
+})
+
 const player = new Fighter({
     position: {
     x: 0,
@@ -79,6 +89,7 @@ function animate() {
     c.fillStyle = "black"
     c.fillRect(0,0, canvas.width, canvas.height)
     background.update()
+    shop.update()
     player.update()
     enemy.update()
 
